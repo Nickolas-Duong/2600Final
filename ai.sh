0c7658648f44e7e4c367ec5c2291a9a07b6eddac
nduong <nduong@cpp.edu>
2022-12-15 11:13:30 -0800

    Work on startup

--- ai.sh
+++ ai.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-./test &
-(sleep 10 && mqttx pub -t 'esp32/tictac' -h 'broker.emqx.io' -p 1883 -m "1") &
+echo "Enter 'player' for player v player"
 
+read -t 5 choice
+  
+if [[ $choice == "player" ]]; then
+	./test & 
+	sleep 2 && mqttx pub -t 'esp32/tictac' -h 'broker.emqx.io' -p 1883 -m "p" &
+else
+	./test &
+	 sleep 2 && mqttx pub -t 'esp32/tictac' -h 'broker.emqx.io' -p 1883 -m "a" &
+fi
---
+++ startup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+gnome-terminal --working-directory /home/krievoblast/Desktop/hwtwo/2600Final -x bash -c "./ai.sh; exec bash"
+
