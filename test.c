3099b38169ec58a3c2baf791c84a313f17414a96
Nickolas Duong <nduong@cpp.edu>
2022-12-15 09:53:08 -0800

    Change player input fix

--- test.c
+++ test.c
@@ -80,12 +80,12 @@ int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *m
 
     if(ai == false && player == false)
     {
-        if(*curmsg == "ai")
+        if(*curmsg == "a")
         {
             printf("\nGame set to Player VS AI...\n");
             ai = true;
         }
-        else if (*curmsg == "player")
+        else if (*curmsg == "p")
         {
             printf("\nGame set to Player VS Player");
             player = true;
