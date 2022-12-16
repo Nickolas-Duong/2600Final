5c29f8510bf5eae30c9e4ddabe5e252a66644869
nduong <nduong@cpp.edu>
2022-12-16 00:12:38 -0800

    Update code for automation and player input

--- ai.sh
+++ ai.sh
@@ -3,11 +3,35 @@
 echo "Enter 'player' for player v player"
 
 read -t 5 choice
-  
+
+count=0
+
 if [[ $choice == "player" ]]; then
-	./test & 
-	sleep 2 && mqttx pub -t 'esp32/tictac' -h 'broker.emqx.io' -p 1883 -m "p" &
+	sleep 2 && mqttx pub -t 'esp32/tictac' -h 'broker.emqx.io' -p 1883 -m "p"
+	while [ $count -le 8 ];
+	do
+		#sleep 10
+		echo "Enter 1-9 to place an O | Enter 0 to quit the game"
+		read num
+		if [[ $num == "0" ]]; then
+			count=$(($count + 10))
+			echo "Quitting..."
+		else
+			mqttx pub -t 'esp32/tictac' -h 'brokeer.emqx.io' -p 1883 -m $num
+			count=$(($count + 1))
+			echo $num
+		fi
+	done
+	echo $count
 else
-	./test &
-	 sleep 2 && mqttx pub -t 'esp32/tictac' -h 'broker.emqx.io' -p 1883 -m "a" &
+	sleep 2 && mqttx pub -t 'esp32/tictac' -h 'broker.emqx.io' -p 1883 -m "a" &
+	while [ $count -le 8 ];
+	do
+		sleep 10
+		num=$(($RANDOM % 9 + 1))
+		mqttx pub -t 'esp32/tictac' -h 'broker.emqx.io' -p 1883 -m $num
+		count=$(($count + 1))
+		echo $num
+	done
+	echo $count
 fi
